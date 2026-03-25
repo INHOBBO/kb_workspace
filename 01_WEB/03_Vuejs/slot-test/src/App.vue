38f48c89d8ba6c320381a2d56d21fef857e782a5
Inho Kang <rkddlsgh99@gmail.com>
2026-03-25 13:45:48 +0900

    260325

---
+++ 01_WEB/03_Vuejs/slot-test/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <NoSlotTest />
+  </div>
+  <div>
+    <SlotTest />
+  </div>
+</template>
+
+<script>
+import NoSlotTest from './components/NoSlotTest.vue';
+import SlotTest from './components/SlotTest.vue';
+
+export default {
+  name: 'App',
+  components: { NoSlotTest, SlotTest },
+};
+</script>
